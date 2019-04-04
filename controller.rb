e517907025f7963e7380e3ebae676f279e7941c4
itggot-Axel-Forsberg <35598696+itggot-Axel-Forsberg@users.noreply.github.com>
2019-04-04 10:51:33 +0200

    Funktioner
    Jag har arbetat med min login och min register funktion inuti model.rb och försökt få dem att fungera.

--- controller.rb
+++ controller.rb
@@ -3,17 +3,28 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
+#enable :sessions
+require_relative('model.rb')
 
-def login()
-    db = SQLite3::Database.new('db/Users.db')
-    db.results_as_hash = true
-    pass_crypt = db.execute("SELECT Password, User_Id FROM Users WHERE Username = ?",params["Username"])
-    
-    if (BCrypt::Password.new(pass_crypt.first["Password"]) == params["Password"]) == true
-        session[:User] = params["Username"]
-    
-        logged_in= true
-    else
-        logged_in= false
-    end
+get('/') do
+    slim(:index)
+end
+
+get('/register') do
+    slim(:register)
+end
+
+post('/register') do
+    register(params)
+
+    redirect('/login')
+end
+
+get('/login') do
+    slim(:login)
+end
+
+post('/login') do
+    session[:User], session[:User_Id] = login(params)
+    redirect('/')
 end
--- model.rb
+++ model.rb
@@ -3,11 +3,34 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
-#enable :sessions
 
-
-get('/') do
-    slim(:index)
+def connect_db()
+    db = SQLite3::Database.new('db/store.db')
+    db.results_as_hash = true
+    return db
 end
 
-get('')
+def register(params)
+    if params["Username"] && params["Email"] && params["Password"] != nil
+
+        db = connect_db()
+        hashat_password = BCrypt::Password.create(params["Password"])
+
+        db.execute("INSERT INTO Username, Email, Password VALUES(?, ?, ?)", params["Username"], params["Email"], hashat_password)
+        return true
+    else
+        return false
+    end
+end
+
+def login(params)
+    db = SQLite3::Database.new('db/Users.db')
+    db.results_as_hash = true
+    pass_crypt = db.execute("SELECT Password, User_Id FROM Users WHERE Username = ?",params["Username"])
+    
+    if (BCrypt::Password.new(pass_crypt.first["Password"]) == params["Password"]) == true
+        return params["Username"], params["User_Id"]
+    else
+        return false
+    end
+end
