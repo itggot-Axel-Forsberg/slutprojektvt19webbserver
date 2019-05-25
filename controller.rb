e896ae8c55d23631780baff2a3eeb61e8b3dbec7
Axel Forsberg <35598696+itggot-Axel-Forsberg@users.noreply.github.com>
2019-05-25 15:27:35 +0200

    Fortsatte dokumentering och lade till funktioner.
    Jag fortsatte att dokumentera funktioner på sidan. Jag gjorde också en sida där användaren kan se sina orders och så lade jag till en funktion så man kan ta bort enskilda produkter ur sin aktuella order.

--- controller.rb
+++ controller.rb
@@ -6,13 +6,12 @@ require 'bcrypt'
 enable :sessions
 require_relative 'model.rb'
 include MyModule
-#Model::Post.create_post(data)
-
-# Display Landing Page
-#
-
 
 helpers do
+    # Selects and writes the error text corresponding to the type of error into session.
+    # 
+    # @error_type [String] String describing the type of error.
+    #
     def get_error(error_type)
         if error_type == "registererror"
             session[:error] = "You entered incorrect register credentials. Please try again."
@@ -23,18 +22,24 @@ helpers do
         end
     end
 end
-#Checks if user is logged in before entering some selected routes.
+# Sets which routes that are secured.
 #
 configure do
-    set :secured_paths,["/cart", "/checkout","/store/"]
+    set :secured_paths,["/cart", "/checkout","/store/","/profile", "/delete/"]
 end
 
+# Checks if user is logged in before entering some selected routes.
+#
+# @session [Integer]:User_Id, the id of the user.
+#
 before do 
     if settings.secured_paths.any? {|elem| request.path.start_with?(elem)} 
         halt 403 unless session[:User_Id]
     end
 end
 
+# Display Landing Page
+#
 get('/') do
     slim(:index)
 end
@@ -45,26 +50,26 @@ get('/register') do
     slim(:register)
 end
 
-get('/register_error') do 
-    slim(:register_error)
-end
 # Attempts to register
 # @param [String] Username, The username
 # @param [String] Email, the users Email 
 # @param [String] Password, The password
 #
 # @see Model#register
+# @see get_error
 post('/register') do
     result = register(params)
 
     if result == true
         redirect('/login')
     else
+        result = "registererror"
         get_error(result)
 
         redirect('/register')
     end
 end
+
 # Displays a login form
 #
 get('/login') do
@@ -75,39 +80,55 @@ end
 # @param [String] Username, The username
 # @param [String] Password, The password
 #
-# see Model#login
+# @see Model#login
+# @see get_error
 post('/login') do
     result = login(params)
-    if result[2] == true
-        session[:User], session[:User_Id] = login(params)
-        redirect('/')   
-    else
+    if result == false
+        result = "loginerror"
         get_error(result)
         redirect('/login')
+    else
+        session[:User], session[:User_Id] = login(params)
+        redirect('/')   
     end
 end
 
-# Displays login form with error message
+# Logs the user out by resetting session values.
 #
-get('/login_error') do
-    slim(:login_error)
+post('/logout') do
+    session[:User_Id] = nil
+    session[:User] = nil
+    redirect('/')
 end
 
-# Displays items that can be purchased from database
+# Displays profile page containing orders the user has made.
 #
+# @see Model#user_orders
+get('/profile') do
+    user_orders = user_orders(session[:User_Id])
+
+    slim(:profile, locals:{orders:user_orders})
+end
+
+# Displays items that can be purchased from database.
+#
+# @see Model#item_info
 get('/store') do
-    db = connect_db()
     item = item_info()
 
     slim(:store, locals:{products:item})
 end
 
-# Adds a product as an order
+# Adds a product to the orderlist to a new or already existing order.
 # 
 # @param [String] Name, Name of product
 # @param [Int] Price, Price of the product
 # @param [Int] Username, The username
 #
+# @see Model#new_order
+# @see Model#add_orderitem
+# @see get_error
 post('/store/:item_id') do
 
     session[:orderid] = new_order(session[:User_Id])
@@ -115,36 +136,57 @@ post('/store/:item_id') do
     if result == true
         redirect('/cart')
     else
+        result = "itemerror"
         get_error(result)
         redirect('/store')
     end
 end
 
-# 
+# Displays items and price from the users active order. Creates an active order if there is none.
+#
+# @see Model#new_order
+# @see Model#order_info
 get('/cart') do 
-    #VEM är inloggad?
-    #vad har hen för cart?
-    #hämta cart
-    #hämta orderitems för cart
+    if session[:orderid] == nil
+        session[:orderid] = new_order(session[:User_Id])
+    end
     order = orderinfo(session[:orderid])
         
     slim(:cart, locals:{cart:order})
-    
 end
 
+# Deletes an item from the users active order.
+#
+# @param [Integer] item_id, The id of the item to be delted from the order.
+#
+# @see Model#delete_orderitem
+post('/delete/:item_id') do
+    delete_orderitem(params["item_id"], session[:orderid], session[:User_Id])
+    redirect('/cart')
+end
+
+# Redirects user to checkout page.
+#
 post('/cart') do
     redirect('/checkout')
 end
 
+# Displays a checkout form.
+#
 get('/checkout') do
     slim(:checkout)
 end
 
+# Finishes the users active order and redirects to home page.
+#
+# @see Model#checkout
 post('/checkout') do
     checkout(session[:User_Id])
     redirect('/')
 end
 
+# Displays error 403 page.
+#
 error 403 do
     slim(:error403)
 end
--- model.rb
+++ model.rb
@@ -4,12 +4,26 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 module MyModule
+
+    # Connects to the database and then returns it.
+    # 
+    # return [Database] containing all the data in database.
     def connect_db()
         db = SQLite3::Database.new('db/store.db')
         db.results_as_hash = true
         return db
     end
-#se till att rätt format skrivs in vid insert funktioner.
+    
+    # Creates a new account unless the either of the params are empty.
+    # 
+    # @param [Hash] params form data from register.
+    # @option params [String] Username, The username
+    # @option params [String] Email, The users email
+    # @option params [String] Password, The users password
+    #
+    # @return [Boolean] if register is successful
+    #
+    # @return [Boolean] if register is not successful
     def register(params)
         unless params["Username"] == "" or params["Email"] == "" or params["Password"] == ""
             if params["Username"] && params["Email"] && params["Password"]
@@ -22,9 +36,18 @@ module MyModule
                 
             end
         end
-        return "registererror"
+        return false
     end
 
+    # Logs the user in to an existing account unless either of the params are empty.
+    #
+    # @param [Hash] params form data from register.
+    # @option params [String] Username, The username
+    # @option params [String] Password, The users password
+    #
+    # @return [Array] containing username, userid and boolean, if login is successful 
+    #
+    # @return [Boolean] if login is unsuccessful
     def login(params)
         db = connect_db()
         user_info = db.execute("SELECT Password, User_Id FROM users WHERE Username = ?",params["Username"])
@@ -35,36 +58,54 @@ module MyModule
                 return [username, user_id, true]
             end
         end
-        return "loginerror"
+        return false
     end
 
+    # Creates a new order unless there already is an active order and returns the orderid of the order.
+    #
+    # @param [Integer] User_Id, The id of the user 
+    # 
+    # @return [Integer] orderid["Order_Id"], the id of the active order.
     def new_order(user_id)
         db = connect_db()
         status = "unpaid"
         if db.execute("SELECT * FROM orders WHERE User_Id = ? AND Status = ?", user_id, status) == []
-            #ta datum och sätt in
+            
             db.execute("INSERT INTO orders(User_Id, Price, Status) VALUES(?, 0, ?)", user_id, status)
-        else
         end
         orderid = db.execute("SELECT Order_Id FROM orders WHERE User_Id = ? AND Status = ?", user_id, status).first
         return orderid["Order_Id"]
     end
 
+    # Adds an item, price and amount to an existing order unless the amount is 0 or lower.
+    #
+    # @param [Hash] params form data from store page.
+    # @option params [Integer] Amount, the amount of a specific product.
+    # @option params [String] Item_Id, The id of the product that is to be added.
+    #
+    # @return [Boolean] if product is added.
+    #
+    # @return [Boolean] if product is not added.
     def add_orderitem(params, user_id)
         status = "unpaid"
         if params[:amount].to_i > 0
             db = connect_db()
+            status = "unpaid"
             order = db.execute("SELECT Order_Id, Price FROM orders WHERE User_Id = ? AND Status = ?", user_id, status).first
             new_item = db.execute("SELECT Item_Name, Price FROM items WHERE Item_Id = ?", params[:item_id]).first
-            db.execute("INSERT INTO orderitem(Order_Id, Item_Id, Order_Name, Price, Amount) VALUES(?, ?, ?, ?, ?)", order["Order_Id"], params[:item_id].to_i, new_item["Item_Name"], new_item["Price"], params[:amount].to_i)
+            price = new_item["Price"] * params[:amount].to_i
+            db.execute("INSERT INTO orderitem(Order_Id, Item_Id, Order_Name, Price, Amount) VALUES(?, ?, ?, ?, ?)", order["Order_Id"], params[:item_id].to_i, new_item["Item_Name"], price, params[:amount].to_i)
             order["Price"] += new_item["Price"] * params[:amount].to_i
-            db.execute("UPDATE orders SET Price=?", order["Price"])
+            db.execute("UPDATE orders SET Price=? WHERE User_Id = ? AND Status = ?", order["Price"], user_id, status)
             return true
         else
-            return "itemerror"
+            return false
         end
     end
 
+    # 
+    #
+    #
     def item_info()
         db = connect_db()
         items = db.execute("SELECT * FROM items")
@@ -73,7 +114,7 @@ module MyModule
 
     def orderinfo(orderid)
         db = connect_db()
-        orderinfo = db.execute("SELECT Order_Id, Order_Name, Price, Amount FROM orderitem WHERE Order_Id = ?", orderid)
+        orderinfo = db.execute("SELECT * FROM orderitem WHERE Order_Id = ?", orderid)
         return orderinfo
     end
 
@@ -82,5 +123,29 @@ module MyModule
         status_unpaid = "unpaid"
         status_paid = "paid"
         db.execute("UPDATE orders SET Status = ? WHERE User_Id = ? AND Status = ?", status_paid, user_id, status_unpaid)
+    end
+
+    def user_orders(user_id)
+        db = connect_db()
+        if db.execute("SELECT * FROM orders WHERE User_Id = ?", user_id) == []
+            user_orders = []
+        else
+            user_orders = db.execute("SELECT Date, Status, Price, Order_Id FROM orders WHERE User_Id = ?", user_id)
+        end
+        return user_orders
+    end
+
+    def delete_orderitem(item_id, orderid, userid)
+        db = connect_db()
+        total = 0
+        status = "unpaid"
+        db.execute("DELETE FROM orderitem WHERE Item_Id = ? AND Order_Id = ?", item_id, orderid)
+        price = db.execute("SELECT Price FROM orderitem WHERE Order_Id = ?", orderid)
+        unless price == nil
+            price.each do |prices|
+                total += prices["Price"]
+            end
+        end
+        db.execute("UPDATE orders SET Price=? WHERE User_Id = ? AND Status = ?", total, userid, status)
     end
 end
